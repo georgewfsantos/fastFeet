d34d9f9ca25bcf5b9403976b34bef3a3fe0ad857
George W F Santos <gweverton21@hotmail.com>
2020-03-23 15:50:40 -0300

    add conditional color for span element

--- frontend/src/pages/Orders/ListOrders/index.js
+++ frontend/src/pages/Orders/ListOrders/index.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import { Container, Title, ListHeader, OrderList, ListItem } from './styles';
 
 export default function ListOrders() {
-  return <div />;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function loadOrders() {
+      const response = await api.get('/orders');
+
+      response.data.forEach(order => {
+        if (order.start_date) {
+          order.status = 'RETIRADA';
+          order.color = '#4D85EE';
+        }
+        if (order.start_date && order.end_date) {
+          order.status = 'ENTREGUE';
+          order.color = '#2CA42B';
+        }
+        if (order.canceled_at) {
+          order.status = 'CANCELADA';
+          order.color = '#DE3B3B';
+        }
+        if (!order.start_date) {
+          order.status = 'PENDING';
+          order.color = '#C1BC35';
+        }
+      });
+
+      setOrders(response.data);
+      console.log(response.data);
+    }
+    loadOrders();
+  }, []);
+  return (
+    <Container>
+      <Title>Gerenciando encomendas</Title>
+      <Input id="buscar" name="buscar" />
+
+      <OrderList>
+        <ListHeader>
+          <div className="order_id">ID</div>
+          <div className="addressee">Destinatário</div>
+          <div className="deliverer">Entregador</div>
+          <div className="city">Cidade</div>
+          <div className="state">Estado</div>
+          <div className="status">Status</div>
+          <div className="actions">Ações</div>
+        </ListHeader>
+        {orders.map(order => (
+          <ListItem key={order.id}>
+            <div id="order_id">{`#${order.id}`}</div>
+            <div id="addressee">{order.addressee.name}</div>
+            <div id="deliverer">{order.deliverer.name}</div>
+            <div id="city">{order.addressee.city}</div>
+            <div id="state">{order.addressee.city}</div>
+            <div id="status">
+              <span color={order.color}>{order.status}</span>
+            </div>
+            <div id="actions">...</div>
+          </ListItem>
+        ))}
+      </OrderList>
+    </Container>
+  );
 }
---
+++ frontend/src/pages/Orders/ListOrders/styles.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  align-self: center;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 20px;
+  margin-top: 15px;
+
+  width: 100%;
+  max-width: 1000px;
+
+  input {
+    align-self: flex-start;
+    margin-top: 20px;
+    height: 36px;
+    width: 250px;
+    margin-bottom: 15px;
+    padding: 15px;
+    border: #dddddd;
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-self: flex-start;
+  font-size: 24px;
+  font-weight: bold;
+  color: #444444;
+`;
+export const ListHeader = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1000px;
+  align-content: center;
+  justify-content: space-between;
+
+  padding: 0 20px;
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 16px;
+  color: #444444;
+
+  > div {
+    width: 14%;
+  }
+
+  .addressee,
+  .deliverer {
+    width: 25%;
+  }
+
+  .actions {
+    justify-self: flex-end;
+
+    text-align: end;
+  }
+`;
+export const OrderList = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1000px;
+  flex-direction: column;
+`;
+export const ListItem = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1000px;
+  align-items: center;
+  justify-content: space-between;
+  background: #fff;
+  padding: 0 20px;
+  margin-top: 10px;
+  height: 64px;
+  border-radius: 4px;
+  font-size: 16px;
+  color: #666666;
+
+  > div {
+    width: 14%;
+    padding: 4px;
+  }
+
+  > div > span {
+    font-size: 14px;
+    font-weight: bold;
+    color: ${props => (props.status ? props.status : '')};
+  }
+
+  #addressee,
+  #deliverer {
+    width: 25%;
+  }
+
+  #actions {
+    justify-self: flex-end;
+
+    text-align: end;
+    margin-right: 5px;
+  }
+`;
