b948b143ae246219c5b38217f99e1933bac5cde4
George W F Santos <gweverton21@hotmail.com>
2020-04-29 18:31:39 -0300

    start implementing react-native-camera use

--- mobile/src/pages/SetAsDelivered/index.js
+++ mobile/src/pages/SetAsDelivered/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useRef} from 'react';
+import {StatusBar} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-// import { Container } from './styles';
+import {Container} from './styles';
+import HeaderBackground from '~pages/Deliveries/Components/HeaderBackground';
 
 export default function SetAsDelivered() {
-  return <View />;
+  const cameraRef = useRef(null);
+  return (
+    <>
+      <StatusBar backgroundColor="#7D40E7" barStyle="light-content" />
+      <HeaderBackground />
+      <Container>
+        <RNCamera ref={cameraRef} />
+      </Container>
+    </>
+  );
 }
